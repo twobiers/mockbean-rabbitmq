664cda811438f26ce2efc363a33aeaf4f17eb462
twobiers <22715034+twobiers@users.noreply.github.com>
2023-01-13 18:09:47 +0100

    fix output

--- src/main/java/com/example/mockbeantest/MessageConsumer.java
+++ src/main/java/com/example/mockbeantest/MessageConsumer.java
@@ -5,6 +5,8 @@ import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.aop.framework.AopProxyUtils;
+import org.springframework.aop.support.AopUtils;
 import org.springframework.stereotype.Component;
 
 @Component
